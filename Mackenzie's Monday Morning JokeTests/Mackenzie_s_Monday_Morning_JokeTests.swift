e4b04f269f20c0f7b2e842adeacd30623e7d6fb3
Mackenzie Fernandez <mackenzie@dragondev.com>
2015-08-27 07:48:16 -0600

    Initial Commit

---
+++ Mackenzie's Monday Morning JokeTests/Mackenzie_s_Monday_Morning_JokeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mackenzie_s_Monday_Morning_JokeTests.swift
+//  Mackenzie's Monday Morning JokeTests
+//
+//  Created by Mackenzie Fernandez on 8/18/15.
+//  Copyright (c) 2015 Mackenzie Fernandez. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Mackenzie_s_Monday_Morning_JokeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
